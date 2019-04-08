c9173381c4dc6963cccf05394a645c47f0db7224
jansule <j_sule02@uni-muenster.de>
2019-04-08 15:56:28 +0200

    [TEST]

--- .ci/update-gh-pages.sh
+++ .ci/update-gh-pages.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 set -ex
 
-echo "TRAVIS: $TRAVIS"
-echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
-echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
-echo "TRAVIS_TAG: $TRAVIS_TAG"
+if [ "$TRAVIS" != "true" ]; then
+  # Only do something on travis.
+  echo "This script is supposed to be run inside the travis environment."
+  return 1;
+fi
+
+if [ $TRAVIS_PULL_REQUEST != "false" ]; then
+  # Dont build anything for PR requests, only for merges.
+  return 0;
+fi
+
+if [ $TRAVIS_BRANCH != "master" ]; then
+  # Only update when the target branch is master.
+  return 0;
+fi
+
+if [ -z "$TRAVIS_TAG" ]; then
+  # Only update when a tag was pushed / a new release was published
+  return 0;
+fi
+
+echo "updating gh-pages"
 
